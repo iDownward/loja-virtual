768a7695f74e98f09938a620245446aab8a65196
Matheus Henrique Cunha De Oliveir <matheus.oliveira@ibm.com>
2019-08-17 11:50:29 -0300

    first commit

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ const App = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                Editar <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
               </Text>
             </View>
